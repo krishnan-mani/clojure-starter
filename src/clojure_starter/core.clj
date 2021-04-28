b89b0e9eb35a4a20ce304624f6e8e49f45c30a5a
Krishnan M <km@krishnanm.com>
2021-04-28 15:57:18 +0100

    introduced pending test

--- src/clojure_starter/core.clj
+++ src/clojure_starter/core.clj
@@ -1,6 +1,11 @@
 (ns clojure-starter.core
   (:gen-class))
 
+(defn my-add [x y]
+  (+ x y))
+
+(defn foo [] true)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/clojure_starter/core_test.clj
+++ test/clojure_starter/core_test.clj
@@ -5,3 +5,7 @@
 (deftest a-test
   (testing "FIXME! I fail!"
     (is (= 0 1))))
+
+
+(deftest ^:kaocha/pending another-test
+  (is (= 3 (my-add 1 2))))
