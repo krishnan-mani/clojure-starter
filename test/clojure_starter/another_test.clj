75497e9ea78563615d8bac6683f2aaf5e8bf8e6f
Krishnan M <km@krishnanm.com>
2021-04-30 12:02:11 +0100

    did first problem to count runs in string

--- test/clojure_starter/another_test.clj
+++ test/clojure_starter/another_test.clj
@@ -3,15 +3,15 @@
             [clojure-starter.core :refer :all]))
 
 
-(deftest test-foo
-  (is (= true (foo)))
-  )
-
-(deftest ^:skip example-skipped-test
-  (is (= 47 (+ 1 2)))
-  )
-
-; change the meta to ":only" to focus on the test below
-(deftest ^:not-only example-focus-test
-  (is (= 21 (+ 7 3)))
-  )
+;(deftest test-foo
+;  (is (= true (foo)))
+;  )
+;
+;(deftest ^:skip example-skipped-test
+;  (is (= 47 (+ 1 2)))
+;  )
+;
+;; change the meta to ":only" to focus on the test below
+;(deftest ^:not-only example-focus-test
+;  (is (= 21 (+ 7 3)))
+;  )
--- test/clojure_starter/core_test.clj
+++ test/clojure_starter/core_test.clj
@@ -2,10 +2,30 @@
   (:require [clojure.test :refer :all]
             [clojure-starter.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME! I fail!"
-    (is (= 0 1))))
+
+(defn describe-string-run [coll]
+  (let [char (first coll)
+        count (count coll)]
+    {:char (str char) :count count}))
+
+(defn collect-runs [x]
+  (let [string-split-by-same-character (partition-by identity x)]
+    (map describe-string-run string-split-by-same-character)))
+
+(deftest describe-run-test
+  (is (= {:char "T" :count 3} (describe-string-run [\T \T \T])))
+  )
+
+(deftest collect-runs-test
+  (is (= [{:char "z" :count 2} {:char "T" :count 3} {:char "y" :count 1}] (collect-runs "zzTTTy")))
+  (is (= [{:char "T" :count 3}] (collect-runs "TTT")))
+  )
 
 
-(deftest ^:kaocha/pending another-test
-  (is (= 73 (my-add 1 2))))
+;(deftest a-test
+;  (testing "FIXME! I fail!"
+;    (is (= 0 1))))
+;
+;
+;(deftest ^:kaocha/pending another-test
+;  (is (= 73 (my-add 1 2))))
